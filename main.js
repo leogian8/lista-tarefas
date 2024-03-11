e2e7ce4feccd836a2e647e0a1728c5cd3e55909f
Leo Ellis <leo-gian@hotmail.com>
2024-03-11 12:01:27 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        const valorinput = $("#tarefa").val();
+        $("ul").append(`<li><span class="pendente">📍</span> ${valorinput}</li>`);
+        $("#tarefa").val('');
+
+        $("ul").on('click', 'li', function(){
+            $(this).toggleClass("linha-meio");
+            $(this).toggleClass("checked");
+            $(this).find(".pendente").toggleClass("hide");
+            
+        })
+    });
+});
